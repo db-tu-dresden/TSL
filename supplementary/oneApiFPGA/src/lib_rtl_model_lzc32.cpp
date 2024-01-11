69cfdc0c8d22b0bbd586a26c626a538ae197455d
Piotr Ratuszniak <piotr.ratuszniak@intel.com>
2024-01-11 17:52:47 +0000

    fixed sw(cpp) model for LZC(RTL based) primitive

--- supplementary/oneApiFPGA/src/lib_rtl_model_lzc32.cpp
+++ supplementary/oneApiFPGA/src/lib_rtl_model_lzc32.cpp
@@ -15,5 +15,17 @@
 #include <CL/sycl.hpp>
 
 SYCL_EXTERNAL extern "C" unsigned Lzc32Uint(unsigned int a) {
-  return a+1; 
+  
+   uint32_t mask = 1 << 31;  
+   uint32_t lzc = 0;
+
+   if (a == 0)
+      lzc = 32;	   
+   else
+      while ((mask & a) == 0)  {
+         mask = mask >> 1;
+         lzc = lzc +1;
+      }  
+  
+   return lzc; 
 }
